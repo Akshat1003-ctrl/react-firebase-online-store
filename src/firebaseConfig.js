7bf473f3b33bedab008b0c1bc38fb1af1db2a6b6
Akshat_031005 <69008358+Akshat1003-ctrl@users.noreply.github.com>
2025-06-10 12:52:01 +0200

    Removed API's

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,18 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace this with your own config object from the Firebase console
+// Replace this with your own config object from the Firebase console
 const firebaseConfig = {
-    apiKey: "AIzaSyCd8OReFzsvNFQwogPlsRk-UR_N6rK5FrI",
-    authDomain: "online-store-6e884.firebaseapp.com",
-    projectId: "online-store-6e884",
-    storageBucket: "online-store-6e884.firebasestorage.app",
-    messagingSenderId: "895462834988",
-    appId: "1:895462834988:web:597859b36bbedd349ce289",
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize and export Firestore so we can use it in other components
 export const db = getFirestore(app);
